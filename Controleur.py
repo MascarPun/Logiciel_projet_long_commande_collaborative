d7c35a0fd6b077e47f4ee5b336db77b0024287ad
Unknown <vic_94@hotmail.fr>
2017-03-14 11:16:44 +0100

    EchelonPosition / ~ProfilPosition

--- Controleur.py
+++ Controleur.py
@@ -9,6 +9,7 @@ import Initialisation_CoMax
 import ctypes
 from ctypes import *
 from matplotlib.pylab import *
+import time
 
 #pwet
 
@@ -19,7 +20,7 @@ class Controleur:
     def __init__(self):
 
         self.parametres = Parametre()
-        self.carteEpos = Initialisation_CoMax.MyEpos
+        self.carteEpos = Initialisation_CoMax.self.carteEpos
         self.interface = Ui_MainWindow(self)
         self.interface.controleur = self
 
@@ -29,8 +30,7 @@ class Controleur:
         self.parametres.setTivit(300)
         self.parametres.getTivit()
         self.parametres.setDureeExp(3)
-        self.parametres.setPosFinale(150)
-        qc2mm = 294
+        mm2qc = 294
 
 
         pErrorCode_i = Initialisation_CoMax.pErrorCode_i
@@ -38,14 +38,6 @@ class Controleur:
         pPositionIs_i = Initialisation_CoMax.pPositionIs_i
         pCurrentIs_i = Initialisation_CoMax.pCurrentIs_i
         pVelocityIs_i = Initialisation_CoMax.pVelocityIs_i
-        Mode = c_int(-1)
-
-#        pErrorCode_i = self.carteEpos.pErrorCode_i
-#        pIsEnabled_i = self.carteEpos.pIsEnabled_i
-#        pPositionIs_i = self.carteEpos.pPositionIs_i
-#        pCulocityIrrentIs_i = self.carteEpos.pCurrentIs_i
-#        pVes_i = self.carteEpos.pVelocityIs_i
-#        Mode = c_int(-1)
 
         NominalCurrent = self.parametres.getNominalCurrent()  # Parametre du logiciel Comax
         MaxOutputCurrent = self.parametres.getMaxOutputCurrent()  # Parametre du logiciel Comax
@@ -54,44 +46,8 @@ class Controleur:
 
         self.carteEpos.setDcMotorParameter(NominalCurrent, MaxOutputCurrent, ThermalTimeConstant, pErrorCode_i)
         self.carteEpos.setMaxAcceleration(MaxAcceleration, pErrorCode_i)
-        self.carteEpos.setOperationMode(Mode, pErrorCode_i)
-
-        pMode = ctypes.POINTER(ctypes.c_int)
-        pMode_i = ctypes.c_int(0)
-        pMode2 = ctypes.cast(ctypes.addressof(pMode_i), pMode)
-        self.carteEpos.getOperationMode(pMode2, pErrorCode_i)
-        self.carteEpos.getOperationMode2(pMode2.contents, pErrorCode_i)
-        print('lala')
-        # set enable state
-        self.carteEpos.setDisableState(pErrorCode_i)
-        self.carteEpos.setEnableState(pErrorCode_i)
-
-        # get enabled state
-        res = self.carteEpos.getEnableState(pIsEnabled_i, pErrorCode_i)
-
-        # Phase de commande du bras pour aller d'une position à une autre
-        pPositionIs = c_long(0)
-        self.carteEpos.getPositionIs(pPositionIs, pErrorCode_i)  # mesure de position initiale
-        positionDepartLueMm = pPositionIs.value / qc2mm  # conversion qc en mm
-
-        Timeout_i = c_long(5000)
-        # On va définir la position initiale
-        #    dlg_title = "Initialisation"
-        #    prompt = {'Position initiale voulue en mm'}
-        #    answer = inputdlg(prompt,dlg_title,1) # création de la boite de dialogue
-        #    positionInitialeMm = str2num(answer{1}) # conversion de la chaine de caractère en entier
-        #    positionInitialeMm = float(input("Position initiale voulue en mm : "))
-        positionFinaleMm = self.parametres.getPosFinale()
 
 
-        if (positionFinaleMm > 500 or positionFinaleMm < 0):  # Il faut définir des conditions de sécurité
-            print('Cette valeur est interdite')
-        else:
-            positionFinaleQc = c_long(math.floor(positionFinaleMm * qc2mm))
-
-            self.carteEpos.setPositionMust(positionFinaleQc, pErrorCode_i)  # on initialise la position à l'origine
-            # définie précédemment. On met 1 en second argument pour un déplacement absolu, 0 pour un déplacement relatif
-        return("fini")
 
     def setMode(self, i):
         self.parametres.setMode(i)
@@ -141,6 +97,126 @@ class Controleur:
     def test(self):
         print("ca marche")
         self.run()
+
+    def echelonPosition(self, posFinale):
+
+        Mode = c_int(-1)
+        self.carteEpos.setOperationMode(Mode, pErrorCode_i)
+        self.parametres.setPosFinale(posFinale)
+
+        pMode = ctypes.POINTER(ctypes.c_int)
+        pMode_i = ctypes.c_int(0)
+        pMode2 = ctypes.cast(ctypes.addressof(pMode_i), pMode)
+        self.carteEpos.getOperationMode(pMode2, pErrorCode_i)
+        self.carteEpos.getOperationMode2(pMode2.contents, pErrorCode_i)
+
+        # set enable state
+        self.carteEpos.setDisableState(pErrorCode_i)
+        self.carteEpos.setEnableState(pErrorCode_i)
+
+        # get enabled state
+        res = self.carteEpos.getEnableState(pIsEnabled_i, pErrorCode_i)
+
+        # Phase de commande du bras pour aller d'une position à une autre
+        pPositionIs = c_long(0)
+        self.carteEpos.getPositionIs(pPositionIs, pErrorCode_i)  # mesure de position initiale
+
+        Timeout_i = c_long(5000)
+        positionFinaleMm = self.parametres.getPosFinale()
+
+
+        if (positionFinaleMm > 500 or positionFinaleMm < 0):  # Il faut définir des conditions de sécurité
+            print('Cette valeur est interdite')
+        else:
+            positionFinaleQc = c_long(math.floor(positionFinaleMm * mm2qc))
+
+            self.carteEpos.setPositionMust(positionFinaleQc, pErrorCode_i)  # on initialise la position à l'origine
+            # définie précédemment. On met 1 en second argument pour un déplacement absolu, 0 pour un déplacement relatif
+        return("fini")
+
+
+    def profilEchelonPosition(self):
+
+        Te = 0.001
+        # On impose le mode Position Mode
+
+        Mode = c_int(1)
+
+        self.carteEpos.setOperationMode(Mode, pErrorCode_i)
+
+        pMode = ctypes.POINTER(ctypes.c_int)
+        pMode_i = ctypes.c_int(0)
+        pMode2 = ctypes.cast(ctypes.addressof(pMode_i), pMode)
+        self.carteEpos.getOperationMode(pMode2, pErrorCode_i)
+        self.carteEpos.getOperationMode2(pMode2.contents, pErrorCode_i)
+
+        # set enable state
+        self.carteEpos.setEnableState(pErrorCode_i)
+
+        # get enabled state
+        res = self.carteEpos.getEnableState(pIsEnabled_i, pErrorCode_i)
+
+        # Phase de commande du bras pour aller d'une position à une autre
+        pPositionIs = c_long(0)
+        self.carteEpos.getPositionIs(pPositionIs, pErrorCode_i)  # mesure de position initiale
+
+        Timeout_i = c_long(5000)
+
+        positionInitialeMm = positionConsigne
+
+        # créer les tableaux pour enregistrer les valeurs
+        TabPosition = []
+        TabVitesse = []
+        TabCourant = []
+        Temps = []
+        t = time.time()
+        while (time.time() - t < dureeExp):
+            if (positionInitialeMm > 500 or positionInitialeMm < 0):  # Il faut définir des conditions de sécurité
+                print('Cette valeur est interdite')
+            else:
+                positionInitialeQc = c_long(math.floor(positionInitialeMm * 294))
+
+                self.carteEpos.moveToPosition(positionInitialeQc, 1, 1, pErrorCode_i)  # on initialise la position à l'origine définie précédemment.
+                #  On met 1 en second argument pour un déplacement absolu, 0 pour un déplacement relatif
+                #            self.carteEpos.waitForTargetReached(Timeout_i, pErrorCode_i)
+                print('Bras en déplacement')
+
+            time.sleep(Te)
+
+            Temps.append(time.time()-t)
+            self.carteEpos.getPositionIs(pPositionIs_i, pErrorCode_i)
+            self.carteEpos.getVelocityIs(pVelocityIs_i, pErrorCode_i)
+            self.carteEpos.getCurrentIs(pCurrentIs_i, pErrorCode_i)
+            TabPosition.append(pPositionIs_i.contents.value / mm2qc)
+            TabVitesse.append(float(pVelocityIs_i.contents.value))
+            TabCourant.append(float(pCurrentIs_i.contents.value))
+
+        # On mesure l'écart à la consigne
+        erreurPositionInitiale = positionInitialeQc.value - pPositionIs.value
+
+        plot(Temps, TabPosition, 'green')
+        grid(True)
+        title('Position instantanée du bras')
+        ylabel('Position (mm)')
+        xlabel('Temps (s)')
+        show()
+
+        plot(Temps, TabVitesse, 'red')
+        grid(True)
+        title('Vitesse instantanée du bras')
+        ylabel('Vitesse (rpm)')
+        xlabel('Temps (s)')
+        show()
+
+        plot(Temps, TabCourant, 'blue')
+        grid(True)
+        title('Courant instantané du bras')
+        ylabel('Courant (mA)')
+        xlabel('Temps (s)')
+        show()
+
+
+
 
 
 
