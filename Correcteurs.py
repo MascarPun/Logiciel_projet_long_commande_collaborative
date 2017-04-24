11e012f89d85947ec6c52d88c3605af8e31af6f0
Unknown <aurelien.dworniczek@gmail.com>
2017-04-24 16:13:43 +0200

    correcteurs_fin
    je pratique le commit de répétition

--- Correcteurs.py
+++ Correcteurs.py
@@ -1,6 +1,6 @@
 #Ensemble des correcteurs du modèle
 #chaque fonction rend un tableau de valeurs
-#veiller a preciser deux conditions initiales
+#ATTENTION NE FONCTIONNE QUE SI LA CARTE EPOS EST INITIALIEE AVANT
 
 import numpy as np
 import matplotlib as mat
@@ -9,6 +9,8 @@ import ctypes
 from ctypes import *
 import Initialisation_CoMax
 from Parametre import Parametre
+
+
 
 #K,Ti,Td les parametres du correcteur
 #Te periode echantillonnage
@@ -40,19 +42,25 @@ def pi_bilin(K,Ti,cmd,E1,S1,Te):
 
 
 #pi mais en version un peu plus bourrin
+#err=tableau avec les erreurs
 def pi(K,Ti,err):
     somme_err=sum(err[k] for k in range(len(err)-1))
     result=K*err[-1]+(K/Ti)*somme_err
     return result
 
+#pid bourrin (mais qui marche bien)
 def pid(K,Ti,Td,err):
     somme_err=sum(err[k] for k in range(len(err)-1))
     delta_err=err[-1]-err[-2]
     result=K*err[-1]+(K/Ti)*somme_err+(K*Td)*delta_err
     return result
 
+
+#correcteur proportionnel
 def prop(K,err):
     return K*err[-1]
+
+
 
 #version de correction qui n'utlise pas de transformation bilinéaire
 #prend en argument un tableau avec les valeurs de la commande et la periode d echantillonnage
@@ -64,56 +72,93 @@ def tic():
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
 
+
+
 def toc():
     import time
     if 'startTime_for_tictoc' in globals():
         return (time.time() - startTime_for_tictoc)
 
+
 #permet d'avoir en sortie un courant qu'il faudra ensuite corriger avec current_cmd()
-def pos2current(K,Ti,Td,cmd,carteEpos,S,courant,err):
+def pos2current(K,Ti,Td,cmd,carteEpos,S,courant,err,pErrorCode_i):
     qc2mm = 294
-    pPositionIs_i = Initialisation_CoMax.pPositionIs_i
-    pErrorCode_i = Initialisation_CoMax.pErrorCode_i
+    pPositionIs = c_long(0)
     carteEpos.getPositionIs(pPositionIs, pErrorCode_i) # mesure de position initiale
     positionDepartLueMm = pPositionIs.value/qc2mm # conversion qc en mm
     S.append(positionDepartLueMm)
     err.append(cmd-S[-1])
-    if Td=='none' and Ti!='none':
+    if Td=='' and Ti!='':
         courant.append(pi(K,Ti,err))
-    elif Td=='none' and Ti=='none':
+    elif Td=='' and Ti=='':
         courant.append(prop(K,err))
     else:
         courant.append(pid(K,Ti,Td,cmd,S,err))
     return [courant,S,err]
 
-def vit2current(K,Ti,Td,cmd,carteEpos,S,courant,err):
-    pErrorCode_i = Initialisation_CoMax.pErrorCode_i
-    pVelocityIs_i = Initialisation_CoMax.pVelocityIs_i
+
+#permet d'avoir en sortie une vitesse qu'il faudra ensuite corriger avec vit2current puis current_cmd()
+#fonction identique à la precedente mais nom différent pour plus de lisibilite
+def pos2velocity(K,Ti,Td,cmd,carteEpos,S,vitesse,err,pErrorCode_i):
+    qc2mm = 294
+    pPositionIs = c_long(0)
+    carteEpos.getPositionIs(pPositionIs, pErrorCode_i) # mesure de position initiale
+    positionDepartLueMm = pPositionIs.value/qc2mm # conversion qc en mm
+    S.append(positionDepartLueMm)
+    err.append(cmd-S[-1])
+    if Td==0 and Ti!=0:
+        vitesse.append(pi(K,Ti,err))
+    elif Td==0 and Ti==0:
+        vitesse.append(prop(K,err))
+    else:
+        vitesse.append(pid(K,Ti,Td,cmd,S,err))
+    return [vitesse,S,err]
+
+
+#conversion de la vitesse en courant
+def velocity2current(K,Ti,Td,cmd,carteEpos,S,courant,err,pErrorCode_i,pVelocityIs_i):
     S.append(carteEpos.getVelocityIs(pVelocityIs_i, pErrorCode_i))
     err.append(cmd-S[-1])
-    if Td=='none' and Ti!='none':
+    if Td==0 and Ti!=0:
         courant.append(pi(K,Ti,err))
-    elif Td=='none' and Ti=='none':
+    elif Td==0 and Ti==0:
         courant.append(prop(K,err))
     else:
         courant.append(pid(K,Ti,Td,cmd,S,err))
     return [courant,S,err]
 
+
+
 #boucle de courant avec pi courant
-def courant_cmd(cmd,S,err,carteEpos,K,Ti,Td,courantC):
-    pErrorCode_i = Initialisation_CoMax.pErrorCode_i
-    pCurrentIs_i = Initialisation_CoMax.pCurrentIs_i
+#S sortie en courant du modele et courantC est le courant en sortie du correcteur
+def courant_cmd(cmd,S,err,carteEpos,K,Ti,Td,courantC,pErrorCode_i,pCurrentIs_i):
     S.append(carteEpos.getCurrentIs(pCurrentIs_i, pErrorCode_i)) #je reprends la notation du controleur
     err.append(cmd-S[-1])
-    if Td=='none' and Ti!='none':
+    if Td==0 and Ti!=0:
         courantC.append(pi(K,Ti,err))
-    elif Td=='none' and Ti=='none':
+    elif Td==0 and Ti==0:
         courantC.append(prop(K,err))
     else:
         courantC.append(pid(K,Ti,Td,cmd,S,err))
     return [courantC,S,err]
 
+#pour l instant bcp d'arguments mais visuellement c est plsu simple a comprendre que des tableaux
+#def regulation_cascade(Kvit,Kpos,Kcour,Tivit,Tipos,Ticour,Tdvit,Tdpos,Tdcour,cmd,errvit,errpos,errcour,Svit,Spos,Scour,courantC,vitesseC,pErrorCode_i,pCurrentIs_i,pVelocityIs_i,carteEpos):
+#    cmdVit=pos2velocity(Kpos, Tipos, Tdpos, cmd, carteEpos, Spos, vitesseC, errpos, pErrorCode_i)[0] #on recupere la vitesse corrigee
+#    cmdCurrent=velocity2current(Kvit,Tivit,Tdvit,cmdVit,carteEpos,Svit,courant,errvit,pErrorCode_i,pVelocityIs_i)
 
+
+
+
+
+#def regulation_vitesse():
+
+
+
+#def regulation_position():
+
+
+#def regulation_courant():
 
 
 
